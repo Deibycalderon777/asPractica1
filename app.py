9f702aabe60de56be733c37f8dee25bec78afb14
Deiby Enrique Calderon <164361682+Deibycalderon777@users.noreply.github.com>
2025-10-07 14:13:20 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from routes.ingrediente_routes import ingrediente_bp
 from routes.usuario_routes import usuario_bp
 
 app = Flask(__name__)
-app.secret_key = 'tu_clave_secreta_aqui_cambiar_en_produccion'  # Necesario para las sesiones
+app.secret_key = 'utnc'  # Necesario para las sesiones
 CORS(app)
 
 # Registrar blueprints
@@ -15,8 +15,12 @@ app.register_blueprint(ingrediente_bp)
 app.register_blueprint(usuario_bp)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def landing_page():
+    return render_template("landing-page.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 @app.route("/login")
 def app_view():
